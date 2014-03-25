313aff7f93dd26a4d1641e0a0ed2b41b0bd42f3f
Aki Suzuki <akisuzucky@gmail.com>
2014-03-24 22:48:14 -0700

    2 - calculate letter grade

--- 2_calculate_letter_grade/my_solution.rb
+++ 2_calculate_letter_grade/my_solution.rb
@@ -4,22 +4,78 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 1. Pseudocode
-
-# What is the input?
-# What is the output? (i.e. What should the code return?)
-# What are the steps needed to solve the problem?
+# DEFINE get_grade method
+# GET array of values
+# Obtain 'sumScore' of numbers in array
+# CREATE variable called 'avgScore', and store output of sum divided by array length
+# CREATE case branches
+# when 90..100 return A
+# when 80..89 return B
+# when 70..79 return C
+# when 60..69 return D
+# when 0..59 return F
 
 
 # 2. Initial Solution
 
+# def get_grade(array)
+# # pop array until a total is derived
+#   arr_size = array.length
+#   i = 0
+#   sumScore = 0
+#   while i < arr_size
+#     sumScore += array.pop
+#     i += 1
+#   end
 
+# # Average the total by dividing total by original array.length
+#   avgScore = sumScore / arr_size
 
+# # Push new number into grade case logic 
+#   case avgScore
+#     when 90..100 then
+#       return "A"
+#     when 80..89 then
+#       return "B"
+#     when 70..79 then
+#       return "C"
+#     when 60..69 then
+#       return "D"
+#     when 0..59 then
+#       return "F"
+#   end
+# end
 
 # 3. Refactored Solution
 
+def get_grade(array)
+	sumScore = 0
+	
+	array.each do |num|
+		sumScore += num
+	end
+	
+	avgScore = sumScore / (array.length)
+
+# Push new number into grade case logic 
+  case avgScore
+    when 90..100 then
+      return "A"
+    when 80..89 then
+      return "B"
+    when 70..79 then
+      return "C"
+    when 60..69 then
+      return "D"
+    when 0..59 then
+      return "F"
+  end
+end
 
 
 # 4. Reflection 
+# Used most of what I learned from exercise 1 to use the array.each do loop to calculate the sum with less code.
+# Really couldn't think of any more compact way to do the case logic. 

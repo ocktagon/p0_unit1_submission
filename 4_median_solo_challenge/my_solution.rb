b212a6d6e9b73b1ac07dd58e8460ea6c47cd808c
Aki Suzuki <akisuzucky@gmail.com>
2014-03-26 01:19:26 -0700

    solo challenge and pad in progress

--- 4_median_solo_challenge/my_solution.rb
+++ 4_median_solo_challenge/my_solution.rb
@@ -4,22 +4,51 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 1. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
-# What are the steps needed to solve the problem?
+# CREATE method 'median' which takes an array as input
+# sort array to alpha-ascending or number num-ascending
+# IF array's length is odd, return array[length divided by 2 minus 1]
+# ELSE 
+#   Find array value of length+1 divided by 2 rounded down to integer
+#   ADD that value with the next value in position to create a sum
+#   Divide the sum by 2 and convert to float to find median
+#   RETURN the median
 
 
 # 2. Initial Solution
-
+# def median(array)
+# 	array.sort!
+# 	if array.length.odd?
+# 		return array[array.length/2]
+# 	else
+# 		firstValue = (array.length+1)/2
+# 		sum = (array[firstValue-1] + array[firstValue])
+# 		return sum/2.to_f
+# 	end
+# end
 
 
 
 # 3. Refactored Solution
 
+def median(array)
+	array.sort!
+	midVar = (array.length + 1) / 2
+	if array.length.odd?
+		return array[midVar - 1]
+	else
+		return (array[midVar - 1] + array[midVar])/2.to_f
+	end
+end
+
 
 
 # 4. Reflection 
+# This exercise took a lot more time than expected, mostly because of two reasons -
+# 1. I didn't realize that array.sort needed the exclamation mark at the end to make the change persist
+# 2. I misrepresented position of the median value vs. it's position in the array.  Took a while to figure out that I only needed to subtract, not add to the value I came up with.
+# It felt good that I got the bonus question for the strings automatically right since I was mainly dealing with returning the value in a position.  I think this code would fail if I had an even number length string array though.  
+
--- 5_pad_array/my_solution.rb
+++ 5_pad_array/my_solution.rb
@@ -1,22 +1,39 @@
 # U1.W3: Pad an Array!
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # Complete each step below according to the challenge directions and 
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 1. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
-# What are the steps needed to solve the problem?
-
+# pad method
+# CREATE Method '#pad' which takes two variables, a num and a pad value
+# IF num input is less than or equal to array.length, do nothing and return the original array
+# ELSE 
+#   DO action (num - array.length) times
+#     push pad value into array
+#   END LOOP
+# RETURN a padded array w/o altering the input
 
 # 2. Initial Solution
+class Array
+	def pad(min_size, value = nil)
+		my_array = Array
+		if min_size == 0
+			return my_array
+		elsif min_size <= my_array.length
+			return my_array
+		else
+		end
+	end
+	def pad!(min_size, value = nil)
 
+	end
+end
 
 
 
@@ -25,3 +42,13 @@
 
 
 # 4. Reflection 
+
+# rspec ./pad_array_spec.rb:12 # Array#pad! operates destructively
+# rspec ./pad_array_spec.rb:16 # Array#pad! doesn't pad a long array
+# rspec ./pad_array_spec.rb:24 # Array#pad operates non-destructively
+# rspec ./pad_array_spec.rb:30 # Array#pad pads elements to the end of an array
+# rspec ./pad_array_spec.rb:38 # Array#pad doesn't pad when minimum size is equal to the array's length
+# rspec ./pad_array_spec.rb:50 # Array#pad pads with nil by default
+# rspec ./pad_array_spec.rb:54 # Array#pad can pad with a string
+# rspec ./pad_array_spec.rb:58 # Array#pad can pad with an object
+
